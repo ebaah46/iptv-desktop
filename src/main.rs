247ae86f1140efaa982baafe735c3f5780d54176
BEKs <ebaah46@yahoo.com>
2026-08-28 19:36:09 +0300

    added slint dependency and created a basic application setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod view_models;
 mod controllers;
 
-fn main() {
-    println!("Hello, world!");
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = MainWindow::new()?;
+    main_window.run()
 }
